59708107615947e949d4b89d91e894526b50e5d3
Edanriell <Edanriell@gmail.com>
2025-11-30 15:34:14 +0200

    feature(fsd-template): implemented

--- Client/svelte.config.js
+++ Client/svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +9,18 @@ const config = {
 	kit: { // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 	// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 	// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-	adapter: adapter() }
+		adapter: adapter(),
+		alias: {
+			"@app": "src/app",
+			"@views": "src/views",
+			"@routes": "src/routes",
+			"@widgets": "src/widgets",
+			"@shared": "src/shared"
+		},
+		files: {
+			appTemplate: "src/app/app.html"
+		}
+	}
 };
 
 export default config;
